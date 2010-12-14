8ddf2cdb0d6bce9540fb097a47f4395773e3fce8
admin <admin@47a30040-fbf1-42e1-bd5d-8610be63dc66>
2010-12-14 16:51:56 +0000

    Adios al viejo engine interprete de plantillas... eliminados ficheros heredados.

--- plantillator/engine/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- vim: expandtab tabstop=4 shiftwidth=4 smarttab autoindent
-
-from .base import VARPATTERN, ParseError, CommandError, Literal
-from .loader import Loader
-from .cmdtree import CommandTree
-from .style import HTMLStyle
-
